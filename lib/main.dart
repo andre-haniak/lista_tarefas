622dbc314a6aba967fc7cf86146d1edc5486a512
unknown <assainks@gmail.com>
2019-10-23 11:16:47 -0300

    Implementado ordenação da lista.

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,22 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 3));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,10 +93,13 @@ class _HomeState extends State<Home> {
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10.0),
-                  itemCount: _toDoList.length,
-                  itemBuilder: buildItem),
+              child: RefreshIndicator(
+                onRefresh: _refresh,
+                child: ListView.builder(
+                    padding: EdgeInsets.only(top: 10.0),
+                    itemCount: _toDoList.length,
+                    itemBuilder: buildItem),
+              ),
             )
           ],
         ));
@@ -132,8 +151,9 @@ class _HomeState extends State<Home> {
                 });
               },
             ),
-            duration: Duration(seconds: 3),
+            duration: Duration(seconds: 2),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
